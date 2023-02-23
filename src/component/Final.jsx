eaf43c916dd5b523a796802ff3ae25c2cdeaee02
SukhdebPatra <95843732+SukhdebPatra@users.noreply.github.com>
2023-02-23 15:32:32 +0530

    Add files via upload

--- src/component/Final.jsx
+++ src/component/Final.jsx
@@ -1,19 +1,13 @@
 import { Typography } from '@mui/material';
 import React from 'react'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+
 
 
 function Final(props) {
   const { formik } = props;
-  // console.log(formik.values)
-  const result = Object.values(formik.values);
-  console.log(result)
+  console.log(formik.values)
+  // const result = Object.values(formik.values);
+  // console.log(result)
   // const data =formik.values
   
 
